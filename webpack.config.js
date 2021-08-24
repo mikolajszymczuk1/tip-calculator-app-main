05e99993e878249e67dba84cd34d6879b5f005a1
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-08-24 03:52:30 +0200

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -9,83 +9,85 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PreloadWebpackPlugin = require("@vue/preload-webpack-plugin");
 
-module.exports = {
-    mode: "development",
-    entry: "./src/ts/index.ts",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {   // HTML
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {   // TypeScript
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-typescript"
-                        ]
-                    }
+module.exports = (env) => {
+    return {
+        mode: env.production ? "production": "development",
+        entry: "./src/ts/index.ts",
+        devtool: "inline-source-map",
+        module: {
+            rules: [
+                {   // HTML
+                    test: /\.html$/i,
+                    loader: "html-loader",
                 },
-            },
-            {   // Sass
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
+                {   // TypeScript
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
                         options: {
-                            implementation: require.resolve("sass"),  // Dart Sass
-                            sourceMap: true,
-                            sassOptions: {
-                                outputStyle: "compressed",
+                            presets: [
+                                "@babel/preset-env",
+                                "@babel/preset-typescript"
+                            ]
+                        }
+                    },
+                },
+                {   // Sass
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                implementation: require.resolve("sass"),  // Dart Sass
+                                sourceMap: true,
+                                sassOptions: {
+                                    outputStyle: "compressed",
+                                }
                             }
                         }
-                    }
-                ]
-            },
-            {   // Images
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {   // Fonts
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index-bundle.js",
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "./src/public/index.html",
-            inject: "body",
-            minify: true,
-        }),
-
-        new MiniCssExtractPlugin(),
-        new PreloadWebpackPlugin({
-            rel: "preload",
-            include: "all",
-        }),
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist"),
+                    ]
+                },
+                {   // Images
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {   // Fonts
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                },
+            ],
         },
-        compress: true,
-        port: 9000,
-    },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "index-bundle.js",
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: "./src/public/index.html",
+                inject: "body",
+                minify: env.m ? true : false,
+            }),
+
+            new MiniCssExtractPlugin(),
+            new PreloadWebpackPlugin({
+                rel: "preload",
+                include: "all",
+            }),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "dist"),
+            },
+            compress: true,
+            port: 9000,
+        },
+    }
 };
